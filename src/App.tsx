e57f49c4d631a843410ed336fd32f57860cbc465
TranspoLord <ascatlover9@gmail.com>
2023-01-21 22:07:36 -0600

    Creation of a few buttons and the starting page
    Co-authored-by: Dan <djk164@yahoo.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import GameManager from "./GameManager"
 
 function App() {
+  const [debug, setDebug] = useState(false)
+  const [startGame, setStartGame] = useState(false)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        !startGame ?
+          <div>
+            <button onClick={
+              () => {setStartGame(!startGame)}
+            } className = "startButton">
+              Start Game
+            </button>
+            <p>Start Game: {startGame ? "True" : "False"}</p>
+            <button onClick={
+              () => {setDebug(!debug)}
+            } className="debugButton">
+              Set Debug Outputs
+            </button>
+            <p>Debug state: {debug ? "True" : "False"}</p>
+          </div>
+          : <GameManager />
+      }
     </div>
   );
 }
