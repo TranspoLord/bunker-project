c5a2169bf182fbd74e362534cf3d185f22a081a1
TranspoLord <ascatlover9@gmail.com>
2023-05-25 21:25:04 -0500

    Restructured repository to have somewhat a structure of folders. Will probably break. Will get it fixed next commit

---
+++ src/BunkerManagement/CreateBunker.jsx
@@ -0,0 +1,233 @@
+import { TextField, Box, Card, Button, CardContent, Typography, FormControl, DialogActions, ListItemText } from '@mui/material';
+import { useState, useContext } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Dialog, DialogTitle, DialogContent, DialogContentText } from '@mui/material';
+import { InputLabel, MenuItem, Select, FormControlLabel, Checkbox } from '@mui/material';
+import { Context } from './SnackBarStoreContext';
+import TestBunkerLogic from './TestBunkerLogic';
+import ItemManagement from './ItemManagement';
+
+function CreateBunker(props) {
+    const [state, dispatch] = useContext(Context);
+    const [bunkerInfoOpen, setBunkerInfoOpen] = useState(false);
+    const { name } = useParams();
+    const getBunker = JSON.parse(localStorage.getItem("bunker-" + name));
+
+    const [bunkerName, setNewBunkerName] = useState(getBunker ? getBunker.name : "");
+    const [bunkerDescription, setNewBunkerDescription] = useState(getBunker ? getBunker.description : "");
+
+    const [bunkerRooms, setRooms] = useState(getBunker ? getBunker.rooms : []);
+    const [newName, setNewName] = useState("");
+    const [newNorth, setNewNorth] = useState("");
+    const [newSouth, setNewSouth] = useState("");
+    const [newEast, setNewEast] = useState("");
+    const [newWest, setNewWest] = useState("");
+    const [newRoomDescription, setNewRoomDescription] = useState("");
+    const [newRoomItem, setNewRoomItem] = useState("None");
+
+    const [bunkerItems, setItems] = useState(getBunker ? getBunker.items : []);
+
+
+
+
+    //Adds a room to the bunkerRooms array
+    function handleAddRoom(e) {
+        e.preventDefault();
+        if (newName === "" || newRoomDescription === "") {
+            dispatch({ type: "OPEN", severity: "error", message: "Room name and description cannot be empty" });
+            return;
+        }
+        if (newNorth === "" && newSouth === "" && newEast === "" && newWest === "") {
+            dispatch({ type: "OPEN", severity: "error", message: "Room must have at least one exit" });
+            return;
+        }
+        const newRoom = {
+            name: newName,
+            north: newNorth,
+            south: newSouth,
+            east: newEast,
+            west: newWest,
+            description: newRoomDescription,
+            item: newRoomItem
+        };
+
+        setRooms([...bunkerRooms, newRoom]);
+        setNewName("");
+        setNewNorth("");
+        setNewSouth("");
+        setNewEast("");
+        setNewWest("");
+        setNewRoomDescription("");
+        setNewRoomItem("");
+    }
+
+    //Deletes a room from the bunkerRooms array
+    function handleRemoveRoom(index) {
+        const newRooms = [...bunkerRooms];
+        newRooms.splice(index, 1);
+        setRooms(newRooms);
+    }
+
+    //Misc handle functions for the dialog boxes
+    const handleBunkerInfoOpen = () => {
+        setBunkerInfoOpen(true);
+    };
+
+    const handleBunkerInfoClose = () => {
+        setBunkerInfoOpen(false);
+    };
+
+
+
+
+    //Builds the JSON object for the bunker
+    function BuildBunker() {
+        const bunker = {
+            name: bunkerName,
+            description: bunkerDescription,
+            items: bunkerItems,
+            rooms: bunkerRooms
+        };
+
+        return bunker;
+    }
+
+    //Saves the bunker to local storage
+    function handleBunkerSave() {
+        console.log("Trying to save bunker to local storage")
+        if (bunkerName === "") {
+            dispatch({ type: "OPEN", severity: "error", message: "Bunker name cannot be empty" });
+            return;
+        }
+        if (bunkerRooms.length < 3) {
+            dispatch({ type: "OPEN", severity: "error", message: "Bunker must have at least 2 rooms" });
+            return;
+        }
+        console.log(bunkerRooms)
+        localStorage.setItem("bunker-" + bunkerName, JSON.stringify(BuildBunker()));
+        dispatch({ type: "OPEN", severity: "success", message: "Bunker saved" });
+    }
+
+    //Handles the logic for editing a room
+    function handleEditRoom(index) {
+        console.log("Wanting to edit room " + index + " " + bunkerRooms[index].name)
+
+        if (newName === "") {
+            const newRooms = [...bunkerRooms];
+            const newRoom = newRooms[index];
+            setNewName(newRoom.name);
+            setNewNorth(newRoom.north);
+            setNewSouth(newRoom.south);
+            setNewEast(newRoom.east);
+            setNewWest(newRoom.west);
+            setNewRoomDescription(newRoom.description);
+            setNewRoomItem(newRoom.item);
+            handleRemoveRoom(index);
+        }
+    }
+
+    //Renders the basic bunker info near the top of the page
+    const getBasicBunkerInfo = () => {
+        return (
+            <div>
+                <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off">
+                    <h2>Basic Bunker Info</h2>
+                    <h4>Rooms: {bunkerRooms.length}</h4>
+                    <TextField required margin="normal" id="outlined-basic" label="Bunker Name" variant="outlined" value={bunkerName} onChange={(e) => setNewBunkerName(e.target.value)} />
+                    <TextField margin="normal" id="outlined-basic" label="Bunker Description" variant="outlined" value={bunkerDescription} onChange={(e) => setNewBunkerDescription(e.target.value)} />
+                </Box>
+            </div>
+        );
+    }
+
+    //Runs a few logic checks to make sure the bunker is valid. Not 100% foolproof, but should catch most errors
+    const handleTestBunker = () => {
+        console.log("Trying to test bunker")
+        dispatch(TestBunkerLogic(BuildBunker()));
+    }
+
+    return (
+        <div>
+            <h1>{getBunker ? "Edit Bunker" : "Create Bunker"}</h1>
+            <Link to="/manage"><Button variant="contained">Back</Button></Link>
+            <Button variant="contained" onClick={handleBunkerSave}>Save Bunker</Button>
+            <Button variant="contained" onClick={handleBunkerInfoOpen}>Bunker Info</Button>
+            <Button variant="contained" onClick={handleTestBunker}>Test Bunker</Button>
+            
+            <ItemManagement setItems={setItems} bunkerItems={bunkerItems}/>
+
+            {/*Contains the dialog box for bunker information/requirements*/}
+            <Dialog open={bunkerInfoOpen} onClose={handleBunkerInfoClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Bunker Info</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        This is a placeholder for bunker info.
+                    </DialogContentText>
+                </DialogContent>
+            </Dialog>
+
+
+            {getBasicBunkerInfo()}
+
+            {/*Contains the input fields for creating a room*/}
+            <h2>Rooms</h2>
+            <Box component="form" sx={{ '& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off">
+                <TextField required size="small" margin="dense" id="outlined-basic" label="Room Name" variant="outlined" value={newName} onChange={(e) => setNewName(e.target.value)} />
+                <TextField size="small" margin="dense" id="outlined-basic" label="Description" variant="outlined" value={newRoomDescription} onChange={(e) => setNewRoomDescription(e.target.value)} />
+                <TextField size="small" margin="dense" id="outlined-basic" label="North Room" variant="outlined" value={newNorth} onChange={(e) => setNewNorth(e.target.value)} />
+                <TextField size="small" margin="dense" id="outlined-basic" label="South Room" variant="outlined" value={newSouth} onChange={(e) => setNewSouth(e.target.value)} />
+                <TextField size="small" margin="dense" id="outlined-basic" label="East Room" variant="outlined" value={newEast} onChange={(e) => setNewEast(e.target.value)} />
+                <TextField size="small" margin="dense" id="outlined-basic" label="West Room" variant="outlined" value={newWest} onChange={(e) => setNewWest(e.target.value)} />
+                <FormControl>
+                    <InputLabel id="RoomItemLabel">Item</InputLabel>
+                    <Select
+                        labelId="RoomItemLabel"
+                        id="select"
+                        value={newRoomItem}
+                        label="Room Item"
+                        onChange={(e) => setNewRoomItem(e.target.value)}>
+                        <MenuItem value={"None"}>None</MenuItem>
+                        {Object.entries(bunkerItems).map(([key, value]) => (
+                            <MenuItem key={key} value={value.name}>{value.name}</MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                <Button variant="contained" onClick={handleAddRoom}>Add Room</Button>
+            </Box>
+
+            {/*Renders the rooms in a card format under room creation*/}
+            {bunkerRooms.map((room, index) => (
+                <Card sx={{ minWidth: 275 }} key={index}>
+                    <CardContent>
+                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                            Room {index + 1}
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                            {room.name}
+                        </Typography>
+                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                            Item: {room.item}
+                        </Typography>
+                        <Typography variant="body2">
+                            North: {room.north}
+                        </Typography>
+                        <Typography variant="body2">
+                            South: {room.south}
+                        </Typography>
+                        <Typography variant="body2">
+                            East: {room.east}
+                        </Typography>
+                        <Typography variant="body2">
+                            West: {room.west}
+                        </Typography>
+                        <Button variant="contained" onClick={() => handleRemoveRoom(index)}>Remove Room</Button>
+                        <Button variant="contained" onClick={() => handleEditRoom(index)}>Edit Room</Button>
+                    </CardContent>
+                </Card>
+            ))}
+        </div>
+    );
+
+}
+
+export default CreateBunker;
---
+++ src/BunkerManagement/ItemManagement.jsx
@@ -0,0 +1,337 @@
+import { React } from 'react';
+import { useState, useContext } from 'react';
+import { Box, ListItemButton } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, FormControlLabel, Checkbox, FormControl, InputLabel, Select, MenuItem, List, ListItemText, Collapse } from '@mui/material';
+import { Context } from './SnackBarStoreContext';
+import { ExpandLess, ExpandMore } from '@mui/icons-material';
+
+function ItemManagement({ bunkerItems, setItems }) {
+    const [state, dispatch] = useContext(Context);
+
+    const [itemInfoOpen, setItemInfoDialogOpen] = useState(false);
+    const [itemCreationOpen, setOpenWindow] = useState(false);
+    const [itemName, setItemName] = useState("");
+    const [itemDescription, setItemDescription] = useState("");
+    const [itemRequired, setItemRequired] = useState("");
+    const [itemPickups, setItemPickups] = useState("");
+    const [alreadyPickedUp, setAlreadyPickedUp] = useState("");
+    const [bacon, setBacon] = useState(false);
+    const [itemNeeded, setItemNeeded] = useState("");
+    const [itemPickupDescription, setItemPickupDescription] = useState("");
+    const [itemIncludeDefaults, setItemIncludePickups] = useState(true);
+    const [itemListOpen, setItemListOpen] = useState(false);
+    const [itemListExpanded, setItemListExpanded] = useState(false);
+    const [itemSecondaryDelete, setItemSecondaryDelete] = useState(false);
+    const [itemIncludeNameInPickups, setItemIncludeNameInPickups] = useState(true);
+
+
+    //Adds an item to the bunkerItems array
+    function handleAddItem(e) {
+        e.preventDefault();
+        if (itemName === "" || itemDescription === "") {
+            dispatch({ type: "OPEN", severity: "error", message: "Item name and description cannot be empty" });
+            return;
+        }
+        const newItem = {
+            name: itemName,
+            description: itemDescription,
+            required: itemRequired,
+            pickups: itemPickups,
+            defaultPickups: itemIncludeDefaults,
+            descAlreadyHave: alreadyPickedUp,
+            descItemNeeded: itemNeeded,
+            descItemPickup: itemPickupDescription,
+            baconItem: bacon,
+        };
+
+        setItems([...bunkerItems, newItem]);
+        setItemName("");
+        setItemDescription("");
+        console.log("Item added" + newItem.name + " " + newItem.description + " " + newItem.required);
+        dispatch({ type: "OPEN", severity: "success", message: "Item added" });
+        handleCloseItem();
+    }
+
+    //Removes an item from the bunkerItems array
+    function handleDeleteItem(index) {
+        const newItems = [...bunkerItems];
+        newItems.splice(index, 1);
+        setItems(newItems);
+    }
+
+    //Handles the logic for including default pickups
+    const handleItemPickupListDefault = () => {
+        console.log("Changing default pickups")
+        if (itemIncludeDefaults) {
+            console.log("Changing to false")
+            if (itemPickups === "") {
+                dispatch({ type: "OPEN", severity: "error", message: "Cannot change default pickups when there are no custom pickups" });
+                setItemIncludePickups(true);
+            }
+            else
+                setItemIncludePickups(false);
+        }
+        else
+            setItemIncludePickups(true);
+
+    };
+
+    //Misc handle functions for the dialog boxes
+    const handleItemOpen = () => {
+        setOpenWindow(true);
+    };
+
+    const handleCloseItem = () => {
+        setOpenWindow(false);
+    };
+
+    const handleItemInfoOpen = () => {
+        setItemInfoDialogOpen(true);
+    };
+
+    const handleItemInfoClose = () => {
+        setItemInfoDialogOpen(false);
+    };
+
+    const handleItemListOpen = () => {
+        setItemListOpen(true);
+    };
+
+    const handleItemListClose = () => {
+        setItemListOpen(false);
+    };
+
+    const handleItemListExpand = () => {
+        setItemListExpanded(!itemListExpanded);
+    };
+
+    const handleItemSecondaryDeleteOpen = () => {
+        setItemSecondaryDelete(true);
+    };
+
+    const handleItemSecondaryDeleteClose = () => {
+        setItemSecondaryDelete(false);
+    };
+
+    return (
+        <>
+            <Button variant="contained" onClick={handleItemOpen}>Create Item</Button>
+            <Button variant="contained" onClick={handleItemListOpen}>Item List</Button>
+
+            {/*Contains the dialog box for item creation*/}
+            <Dialog open={itemCreationOpen} onClose={handleCloseItem} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Create Item</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Create a new item for your bunker.
+                    </DialogContentText>
+                    <form>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="name"
+                            key="name"
+                            label="Name"
+                            required
+                            type="text"
+                            fullWidth
+                            value={itemName}
+                            onChange={(e) => setItemName(e.target.value)}
+                        />
+                        <TextField
+                            margin="dense"
+                            id="description"
+                            key="description"
+                            label="Description"
+                            required
+                            type="text"
+                            fullWidth
+                            value={itemDescription}
+                            onChange={(e) => setItemDescription(e.target.value)}
+                        />
+
+                        <TextField
+                            margin="dense"
+                            id="itemPickupsList"
+                            key="itemPickupsList"
+                            label="Pickup Word List - Seperate with commas"
+                            type="text"
+                            fullWidth
+                            value={itemPickups}
+                            onChange={(e) => setItemPickups(e.target.value)}
+                        />
+                        <div style={{ display: "flex" }}>
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        checked={itemIncludeDefaults}
+                                        onChange={() => handleItemPickupListDefault()}
+                                        name="checkedB"
+                                        color="primary"
+                                        key="itemIncludeDefaults"
+                                    />
+                                }
+                                label="Include Default Pickups"
+                            />
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        checked={itemIncludeNameInPickups}
+                                        onChange={() => setItemIncludeNameInPickups(!itemIncludeNameInPickups)}
+                                        name="checkedC"
+                                        color="primary"
+                                        key="itemIncludeNameInPickups"
+                                    />
+                                }
+                                label="Include Item Name in Pickups"
+                            />
+                        </div>
+                        <TextField
+                            margin="dense"
+                            id="itemPickedUp"
+                            key="itemPickedUp"
+                            label="Picked Up Description"
+                            type="text"
+                            fullWidth
+                            value={itemPickupDescription}
+                            onChange={(e) => setItemPickupDescription(e.target.value)}
+                        />
+                        <TextField
+                            margin="dense"
+                            id="alreadyPickedUp"
+                            key="alreadyPickedUp"
+                            label="Already Picked Up Description"
+                            type="text"
+                            fullWidth
+                            value={alreadyPickedUp}
+                            onChange={(e) => setAlreadyPickedUp(e.target.value)}
+                        />
+                        <TextField
+                            margin="dense"
+                            id="itemNeeded"
+                            key="itemNeeded"
+                            label="Item Needed Description"
+                            type="text"
+                            fullWidth
+                            value={itemNeeded}
+                            onChange={(e) => setItemNeeded(e.target.value)}
+                        />
+                        <FormControl fullWidth>
+                            <InputLabel id="select-label">Item Needed to Pickup</InputLabel>
+                            <Select
+                                labelId="select-label"
+                                id="select"
+                                key="select"
+                                value={itemRequired}
+                                label="Required Item"
+                                onChange={(e) => setItemRequired(e.target.value.name)}
+                            >
+                                {Object.entries(bunkerItems).map(([key, value]) => (
+
+                                    <MenuItem key={key} value={value.name}>{value.name}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={bacon}
+                                    onChange={(e) => setBacon(e.target.checked)}
+                                    name="bacon"
+                                    color="primary"
+                                    key="bacon"
+                                />
+                            }
+                            label="Bacon Item"
+                        />
+                        <DialogActions>
+                            <Button onClick={handleItemInfoOpen}>Item Info</Button>
+                            <Button onClick={handleCloseItem}>Cancel</Button>
+                            <Button onClick={handleAddItem} type="submit">Submit</Button>
+                        </DialogActions>
+                    </form>
+                </DialogContent>
+            </Dialog>
+
+            {/*Contains the dialog box for item information/requirements*/}
+            <Dialog open={itemInfoOpen} onClose={handleItemInfoClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Item Info</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        <h3>Defaults</h3>
+                        <p>Pickup List, Already Picked Up, Item Needed, and Item Pick Up all have default values. If you don't put anything in, they will be set to default. Defaults
+                            are basic pickup words with the descriptions being the item name and the corresponding title.</p>
+                        <h4>Item Name</h4>
+                        <p>Is the name of the item</p>
+                        <h4>Item Description</h4>
+                        <p>Is a short description of the item. This will be played on entry to the room, when the item is available for pickup.</p>
+                        <h4>Include Default Pickups</h4>
+                        <p>If true, the default pickup words will be added to the pickup list. This will not matter if there is no user specified pickup words.</p>
+                        <h4>Item Pickup List</h4>
+                        <p>Is a list of words/sentences that can be used to pickup this item. Seperate each group with a comma.</p>
+                        <h4>Already Picked Up Description</h4>
+                        <p>This is a description that will be played if the player tries to pickup the item again.</p>
+                        <h4>Item Needed Description</h4>
+                        <p>This is a description that will be played if the player tries to aquire the item without having the required item that it needs.</p>
+                        <h4>Item Picked Up Description</h4>
+                        <p>This is a description that will be played when the player picks up the item.</p>
+                        <h4>Bacon Item</h4>
+                        <p>If true, the item will be required to complete the bunker. You can have none or all your items be required.</p>
+                        <h4>Needed to Pickup</h4>
+                        <p>This is the item that is required to pickup this item. If none is needed, leave blank.</p>
+
+                    </DialogContentText>
+                </DialogContent>
+            </Dialog>
+
+            {/*Contains the dialog box for the item list*/}
+            <Dialog open={itemListOpen} onClose={handleItemListClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Item List</DialogTitle>
+                <DialogContent>
+                    <div>
+                        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }} component="nav" aria-label="main item list">
+                            {Object.entries(bunkerItems).map(([key, value]) => (
+                                <div key={key}>
+                                    <Box sx={{ display: 'flex' }}>
+                                        <ListItemText primary={value.name}></ListItemText>
+                                        <ListItemButton onClick={handleItemListExpand}>
+                                            {itemListExpanded ? <ExpandLess /> : <ExpandMore />}
+                                        </ListItemButton>
+                                    </Box>
+                                    <Collapse in={itemListExpanded} timeout="auto" unmountOnExit>
+                                        <List component="div" disablePadding>
+                                            <ListItemButton sx={{ pl: 4, display: 'block' }}>
+                                                <p><b>Description: </b>{value.description}</p>
+                                                <p><b>Pickup Words: </b>{value.pickups}</p>
+                                                <p><b>Already Picked Up Description: </b>{value.descAlreadyHave}</p>
+                                                <p><b>Item Needed Description: </b>{value.descItemNeeded}</p>
+                                                <p><b>Item Picked Up Description: </b>{value.descItemPickup}</p>
+                                                <p><b>Bacon Item: </b>{value.baconItem ? "Yes" : "No"}</p>
+                                                <p><b>Item Required: </b>{value.required}</p>
+                                            </ListItemButton>
+                                            <Button onClick={handleItemSecondaryDeleteOpen}>Delete Item</Button>
+                                            <Dialog open={itemSecondaryDelete} onClose={handleItemSecondaryDeleteClose} aria-labelledby="form-dialog-title">
+                                                <DialogTitle id="form-dialog-title">Delete Item</DialogTitle>
+                                                <DialogContent>
+                                                    <DialogContentText>
+                                                        Are you sure you want to delete this item?
+                                                    </DialogContentText>
+                                                </DialogContent>
+                                                <DialogActions>
+                                                    <Button onClick={handleItemSecondaryDeleteClose}>Cancel</Button>
+                                                    <Button onClick={() => handleDeleteItem(key)}>Delete</Button>
+                                                </DialogActions>
+                                            </Dialog>
+                                        </List>
+                                    </Collapse>
+                                </div>
+                            ))}
+                        </List>
+                    </div>
+                </DialogContent>
+            </Dialog>
+        </>
+    );
+}
+
+export default ItemManagement;
